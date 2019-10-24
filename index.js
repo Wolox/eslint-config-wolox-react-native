7d3598bcae3520f74373810b91c3ab3d6c096890
Mariano Zicavo <marianozicavo@gmail.com>
2019-10-24 12:22:20 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ["@react-native-community", "wolox", "plugin:prettier/recommended"],
+  plugins: ["import"],
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "import/named": "off",
+    "react-native/no-color-literals": "error",
+    "react-native/no-inline-styles": "error"
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx"],
+      parser: "babel-eslint"
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser"
+    },
+    {
+      files: ["styles.ts", "styles.tsx"],
+      rules: {
+        "no-magic-numbers": "off"
+      }
+    }
+  ]
+};
